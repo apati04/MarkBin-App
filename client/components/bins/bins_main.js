b5257ac4aa1fe27f5bda33aa34faf620c68ca314
Andrew Patipaksiri <apax714@gmail.com>
2017-03-18 14:21:50 -0700

    add sharing method on bins

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -3,16 +3,17 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
 import BinsViewer from './bins_viewer';
+import BinsShare from './bins_share';
 
 class BinsMain extends Component {
   render() {
     if(!this.props.bin) { return <div>Loading...</div> }
-
-
+    
     return (
       <div>
         <BinsEditor bin={this.props.bin} />
         <BinsViewer bin={this.props.bin} />
+        <BinsShare bin={this.props.bin} />
       </div>
     );
   }
