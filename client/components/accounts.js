a1ed2684aaa2733660d9bd3e2d8e1e743fcdd2be
Andrew Patipaksiri <apax714@gmail.com>
2017-03-18 10:39:30 -0700

    add views using blaze from meteor

---
+++ client/components/accounts.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'metor/blaze';
+
+class Accounts extends Component {
+  componentDidMount() {
+    // Render Blaze accounts form then find the div
+    // we just rendered in the 'render' method and
+    // the blaze account form in the div.
+    this.view = Blaze.render(Template.longinButtons, ReacDOM.findDOMNode(this.refs.container));
+  }
+  componentWillUnmount() {
+    // find forms and destroy them ourselves.
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Accounts;
