59c3414033d256b5594dcc58c7a8e7affd2b3c99
Andrew Patipaksiri <apax714@gmail.com>
2017-03-18 12:14:58 -0700

    setup default component with indexroute

--- client/main.js
+++ client/main.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/app';
 import BinsMain from './components/bins/bins_main';
-import BinsLIst from './components/bins/bins_list';
+import BinsList from './components/bins/bins_list';
 import { Bins } from '../imports/collections/bins';
 
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={BinsList} />
       <Route path="bins" component={BinsMain} />
     </Route>
   </Router>
