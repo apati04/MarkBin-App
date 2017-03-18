b1657be9d9f15c2893fd3f16d403b6179042fcf1
Andrew Patipaksiri <apax714@gmail.com>
2017-03-18 12:11:43 -0700

    adds new components to router

--- client/main.js
+++ client/main.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/app';
+import BinsMain from './components/bins/bins_main';
+import BinsLIst from './components/bins/bins_list';
 import { Bins } from '../imports/collections/bins';
 
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
+      <Route path="bins" component={BinsMain} />
     </Route>
   </Router>
 )
